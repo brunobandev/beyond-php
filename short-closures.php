709e48a4c0eb71fc0c1e75b1301c278ae38fdf84
Bruno Bandeira <brunoban.dev@gmail.com>
2021-02-03 14:35:35 +0000

    Update short-closures.php

--- short-closures.php
+++ short-closures.php
@@ -11,13 +11,3 @@ print_r($numbersByTwoNormalClousure) . PHP_EOL;
 $numbersByTwoShortClosure = array_map(fn ($x): int => $x * 2, $numbers);
 
 print_r($numbersByTwoShortClosure) . PHP_EOL;
-
-/**
- * Short Closures:
- * They are available as of PHP 7.4
- * They start with the fn keyword
- * They can only have one expression, which is the return statement
- * No return keyword allowed
- * Arguments and return types can be type hinted
- * https://stitcher.io/blog/short-closures-in-php
- */
